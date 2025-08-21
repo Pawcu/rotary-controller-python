09fa82b4484909bcffaba448792d6d159a317806
Pawcu <pawcu@hotmail.co.uk>
2025-08-21 08:35:16 +0200

    Added logic for binding to the scales; Started adding wizard logic

--- rcp/components/home/automatic_threading_bar.py
+++ rcp/components/home/automatic_threading_bar.py
@@ -6,6 +6,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import NumericProperty, BooleanProperty, StringProperty
 
 from rcp.components.home.automatic_threading_settings_popup import AutomaticThreadingSettingsPopup
+from rcp.components.home.automatic_threading_wizard import AutomaticThreadingWizard
+from rcp.components.home.coordbar import CoordBar
 from rcp.dispatchers import SavingDispatcher
 
 log = Logger.getChild(__name__)
@@ -32,17 +34,56 @@ class AutomaticThreadingBar(BoxLayout, SavingDispatcher):
     shaft_diameter = NumericProperty(1)
     
     is_running = BooleanProperty(False)
-    _skip_save = ["is_running"]
+    label_text = StringProperty("")
+    display_value = StringProperty("")
+    next_button_text = StringProperty("")
+    start_position = NumericProperty(0)
+    stop_position = NumericProperty(0)
+    _skip_save = [
+        "is_running",
+        "label_text",
+        "display_value",
+        "start_position"
+        "stop_position"
+        ]
 
     def __init__(self, **kv):
         from rcp.app import MainApp
         self.app: MainApp = MainApp.get_running_app()
         super().__init__(**kv)
+        self.wizard = AutomaticThreadingWizard(self)
     
     def toggle_is_running(self):
         self.is_running = not self.is_running
         self.app.servo.toggle_enable()
+        if self.is_running:
+            self.wizard.start()
+        else:
+            self.wizard.reset_ui()
+
+    def on_wizard_button(self):
+        """Called when the right button is pressed."""
+        if self.is_running:
+            self.wizard.goto_next_step()
+        else:
+            self.open_settings()
     
     def open_settings(self):
         popup = AutomaticThreadingSettingsPopup(automaticThreadingBar=self)
         popup.open()
+        
+    def bind_to_scale(self, scale: CoordBar):
+        """Bind display_value to a scale's formattedPosition."""
+        # Unbind old scale if it exists
+        if hasattr(self, "_bound_scale") and self._bound_scale is not None:
+            self._bound_scale.unbind(formattedPosition=self._update_display_value)
+
+        # Bind new one
+        self._bound_scale = scale
+        scale.bind(formattedPosition=self._update_display_value)
+
+        # Set immediately
+        self.display_value = scale.formattedPosition
+
+    def _update_display_value(self, instance, value):
+        self.display_value = value
---
+++ rcp/components/home/automatic_threading_wizard.py
@@ -0,0 +1,58 @@
+from kivy.logger import Logger
+
+log = Logger.getChild(__name__)
+
+class AutomaticThreadingWizard:
+    def __init__(self, bar):
+        self.bar = bar
+        self.app = bar.app
+        self.current_step = 0
+        self._current_callback = None
+        self.steps = [
+            self.step_1_initial_position,
+            self.step_2_stop_position,
+            # ... same steps as before ...
+        ]
+
+    def start(self):
+        self.goto_step(0)
+
+    def reset_ui(self):
+        # Reset wizard_area to default content
+        self.bar.label_text = ""
+        self.bar.display_value = ""
+
+    def goto_step(self, index):
+        self.current_step = index
+        if 0 <= index < len(self.steps):
+            self.steps[index]()
+        else:
+            log.info("Wizard finished")
+            self.bar.is_running = False
+    
+    def goto_next_step(self, *args):
+        if self._current_callback:
+            self._current_callback(*args)  
+        self.goto_step(self.current_step + 1)
+
+    def set_instruction(self, label_text, next_button_text, next_button_callback):
+        self.bar.label_text = label_text
+        self.bar.next_button_text = next_button_text
+        self._current_callback = next_button_callback
+    
+
+    def step_1_initial_position(self):
+        self.set_instruction("Go to initial Z and press Set", "Set", self._capture_initial_position)
+        self.bar.bind_to_scale(self.app.scales[self.bar.selected_saddle_scale_id])
+
+    def step_2_stop_position(self):
+        self.set_instruction("Go to stop Z and press Set", "Set", None)
+        self.bar.bind_to_scale(self.app.scales[self.bar.selected_saddle_scale_id])
+        
+    def _capture_initial_position(self, *args):
+        self.bar.start_position = self.app.scales[self.bar.selected_saddle_scale_id].position
+        
+    def _capture_stop_position(self, *args):
+        self.bar.stop_position = self.app.scales[self.bar.selected_saddle_scale_id].position    
+    
+        #self.bar.display_value = self.app.servo.formattedPosition
